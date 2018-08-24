0ccaf1cfbe445e782664330370d92c2611847e48
Manon Lan <manondoublier@yahoo.fr>
2018-08-24 03:58:54 +0200

    tests finaux

--- spec/controllers/email_controller_spec.rb
+++ spec/controllers/email_controller_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe EmailController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    test "should have a list of object emails" do
+    assert_select ".emails-object-block"
+end
   end
 
 end
